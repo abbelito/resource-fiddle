2819dab40c275606c874dd930b1ef5f4d6756483
Albert Schapiro <albert@schapiro.se>
2014-11-11 19:06:00 +0100

    Fixed correct address

--- src/client/js/controllers/GraphicsEditor.js
+++ src/client/js/controllers/GraphicsEditor.js
@@ -40,7 +40,7 @@ GraphicsEditor.prototype.onUpload = function(item) {
 		var data = new FormData();
 		data.append('SelectedFile', item.getValues()[0]);
 		data.append("Filename", item.name);
-		data.append("url", this.basePath);
+		data.append("url", document.location + this.basePath);
 		this.currentItem = item;
 		var connection = new APIConnection(this.basePath, this.session);
 		connection.on("loaded", this.onUploaded, this);

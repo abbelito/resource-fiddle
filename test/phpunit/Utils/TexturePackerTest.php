17b295ac1be7c9c26cb6243097e031f8e1b0e8a0
Mikael Lindqvist <li.mikael@gmail.com>
2015-01-11 09:55:33 +0100

    php unit tests

---
+++ test/phpunit/Utils/TexturePackerTest.php
@@ -0,0 +1,10 @@
+<?php
+
+	require_once __DIR__."/../../../src/client/php/Utils/TexturePacker.php";
+
+	class TexturePackerTest extends \PHPUnit_Framework_TestCase {
+
+		function testPack() {
+			$this->assertEquals(TRUE,TRUE);
+		}
+	}
